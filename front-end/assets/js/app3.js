7448decdd53035bf52b778d0a6e8fd9659735943
tiendung2013vn97 <tiendung2013vn97@gmail.com>
2018-11-26 21:15:33 +0700

    updating front end app3

--- front-end/assets/js/app3.js
+++ front-end/assets/js/app3.js
@@ -1,4 +1,18 @@
 $( document ).ready(function() {
+
 	var h=screen.height-150+'px';
     $(".navbar-left").css('height',h);
+
+    $(document).on('click', '#btnDangKi', function() {
+   		$('.modaFormBackground').css('display','block');
+   		$('#dangKiForm form')[0].reset();
+	}) ;
+
+	$(document).on('click', '.modaFormBackground', function() {
+   		 $(".modaFormBackground").css({ "display": "none" });
+	}) ;
+
+	$("#dangKiForm").click(function(e){
+		e.stopPropagation();
+	});
 });
