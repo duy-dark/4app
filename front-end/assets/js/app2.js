86abc78244b8867e96787e694590e10fc96ffaf4
duy-dark <36453006+duy-dark@users.noreply.github.com>
2018-11-28 17:13:48 +0700

    update app2

--- front-end/assets/js/app2.js
+++ front-end/assets/js/app2.js
@@ -32,25 +32,35 @@ function updateaddress() {
         success: function(data) {
 
             alert('update thanh cong');
-            
+
             $('a').each(function(index) {
                 if ($(this).attr('id') === ID) {
                     $(this).css('display', 'none');
                 }
             })
-            $.ajax({
-                url: 'http://localhost:3000/datxe',
-                type: 'GET',
-                dataType: 'json',
-                timeout: 10000
-            }).done(function(data) {
-                // console.log(data);
-                var source = document.getElementById("list-dscd").innerHTML;
-                var template = Handlebars.compile(source);
-                var html = template(data);
-                //var h=$('#appcd').html();
-                $('#appcd').html(html);
-            })
+            var fn = function() {
+                $.ajax({
+                    url: 'http://localhost:3000/datxe',
+                    type: 'GET',
+                    dataType: 'json',
+                    timeout: 10000
+                }).done(function(data) {
+                    // console.log(data);
+                    if (data.YN) {
+                        var request = data.request;
+                        var source = document.getElementById("list-dscd").innerHTML;
+                        var template = Handlebars.compile(source);
+                        var html = template(request);
+                        //var h=$('#appcd').html();
+                        $('#appcd').html(html);
+                    }
+                    else{
+                        setTimeout(fn,2000);
+                    }
+
+                })
+            }
+            fn();
         }
     });
 }
