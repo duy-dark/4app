46112c10a914d9cc390990c53761d3f74c4d69a8
duy-dark <36453006+duy-dark@users.noreply.github.com>
2018-12-05 18:06:52 +0700

    fix bug

--- front-end/assets/js/app4.js
+++ front-end/assets/js/app4.js
@@ -75,18 +75,7 @@ $('#btnDK').click(function() {
 
 });
 
-function hvs2(latA, longA, latB, longB) {
-    var dLat = (latA - latB).toRad();
-    var dLon = (longA - longB).toRad();
-    var dLatDiv2 = dLat / 2;
-    var dLonDiv2 = dLon / 2;
-    var latBRad = latB.toRad();
-    var latBRadCos = Math.cos(latBRad);
-    var dLatDiv2Sin = Math.sin(dLatDiv2);
-    var dLonDiv2Sin = Math.sin(dLonDiv2);
-    var a = dLatDiv2Sin * dLatDiv2Sin + latBRadCos * latBRadCos * dLonDiv2Sin * dLonDiv2Sin;
-    return 6371 * 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
-}
+
 
 function regist() {
     var data = {};
