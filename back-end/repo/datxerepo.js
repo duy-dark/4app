b5aab8c0ceb55c83441e5a8b20897741929ad9cc
tiendung2013vn97 <tiendung2013vn97@gmail.com>
2018-12-05 23:35:22 +0700

    wtf have buf

--- back-end/repo/datxerepo.js
+++ back-end/repo/datxerepo.js
@@ -1,67 +1,49 @@
-var db = require('../fn/mysql-db');
+var db=require('../fn/mysql-db');
 
 
 exports.savekh = KH => {
-    var sql = `insert into chuyendi (HOTENKH,SDT,DIEMDI,GHICHU,STATECD,THOIGIANDAT,STATEREQUEST,TIMEUPDATE)
+	var sql = `insert into chuyendi (HOTENKH,SDT,DIEMDI,GHICHU,STATECD,THOIGIANDAT,STATEREQUEST,TIMEUPDATE)
 				values('${KH.TEN}','${KH.SDT}','${KH.DIEMDI}','${KH.GHICHU}','${KH.STATECD}','${KH.THOIGIANDAT}','chưa định vị',${KH.TIMEUPDATE})`;
-    return db.save(sql);
+	return db.save(sql);
 }
 exports.loadcd = () => {
-    var sql = `select * from chuyendi where STATEREQUEST='chưa định vị'`;
-    return db.load(sql);
+	var sql = `select * from chuyendi where STATEREQUEST='chưa định vị'`;
+	return db.load(sql);
 }
 exports.updatetoado = obj => {
-    var sql = `update chuyendi set TOADON = '${obj.TOADON}', TOADOW = '${obj.TOADOW}', STATEREQUEST = '${obj.STATEREQUEST}', REVERGEOCODING='${obj.REVERGEOCODING}' where IDCD = ${obj.IDCD}`;
-    return db.save(sql);
+	var sql =  `update chuyendi set TOADON = '${obj.TOADON}', TOADOW = '${obj.TOADOW}', STATEREQUEST = '${obj.STATEREQUEST}', REVERGEOCODING='${obj.REVERGEOCODING}' where IDCD = ${obj.IDCD}`;
+	return db.save(sql);
 }
-exports.updatestatecd = (userid, state) => {
-    var sql = `update chuyendi set STATEREQUEST = '${state}'where IDCD = ${userid}`;
-    return db.save(sql);
+exports.updatestatecd = (userid,state) => {
+	var sql =  `update chuyendi set STATEREQUEST = '${state}'where IDCD = ${userid}`;
+	return db.save(sql);
 }
 exports.loadid = IDCD => {
-    var sql = `select * from chuyendi where IDCD=${IDCD}`;
-    return db.load(sql);
+	var sql = `select * from chuyendi where IDCD=${IDCD}`;
+	return db.load(sql);
 }
 
 exports.loadcdapp4 = () => {
-    var sql = `select * from chuyendi where STATECD='chưa nhận'`;
-    return db.load(sql);
+	var sql = `select * from chuyendi where STATECD='chưa nhận'`;
+	return db.load(sql);
 }
 exports.updatestate = IDCD => {
-    var sql = `update chuyendi set STATECD='đã xử lý' where IDCD = ${IDCD}`;
-    return db.load(sql);
+	var sql = `update chuyendi set STATECD='đã nhận' where IDCD = ${IDCD}`;
+	return db.load(sql);
 }
 exports.updatecd = user => {
-    var sql = `update chuyendi set IDTX='${user.IDTX}',STATECD='đã nhận',TIMEUPDATE='${user.TIMEUPDATE}' where IDCD = ${user.IDCD}`;
-    return db.load(sql);
+	var sql = `update chuyendi set IDTX='${user.IDTX}',STATECD='đã nhận',TIMEUPDATE='${user.TIMEUPDATE}' where IDCD = ${user.IDCD}`;
+	return db.load(sql);
 }
 exports.updatestate1 = IDCD => {
-    var sql = `update chuyendi set STATECD='chưa nhận' where IDCD = ${IDCD}`;
-    return db.load(sql);
+	var sql = `update chuyendi set STATECD='chưa nhận' where IDCD = ${IDCD}`;
+	return db.load(sql);
 }
-exports.mangchuatc = mangid => {
-    var sql = `SELECT cd.* FROM chuyendi cd WHERE cd.STATECD='chưa nhận' `;
-   
-    for (i = 0; i < mangid.length; i++) {
-        sql+=` and not EXISTS (SELECT * FROM chuyendi cd1 WHERE cd.IDCD=cd1.IDCD and cd1.IDCD=${mangid[i]})`;
-    }
-    console.log(sql);
-    return db.load(sql);
+exports.getNewRequest=()=>{
+	var sql = `select * from chuyendi where STATEREQUEST='chưa định vị'`;
+	return db.load(sql);
 }
-exports.getNewRequest = () => {
-    var sql = `select * from chuyendi where STATEREQUEST='chưa định vị'`;
-    return db.load(sql);
-}
-exports.updateStateRequest = (row, state) => {
-    var sql = `update chuyendi set STATEREQUEST='${state}' where IDCD=${row.IDCD}`;
-    return db.save(sql);
-}
-
-exports.updateposition = (lat, lng,idtx) => {
-    var sql = `update taixe set TOADON=${lat},TOADOW=${lng} where ID=${idtx}`;
-    return db.save(sql);
-}
-exports.ycrequest = () => {
-    var sql = `select * from taixe where STATE='READY' `;
-    return db.load(sql);
+exports.updateStateRequest=(row,state)=>{
+	var sql = `update chuyendi set STATEREQUEST='${state}' where IDCD=${row.IDCD}`;
+	return db.save(sql);
 }
--- back-end/repo/requestManageRepo.js
+++ back-end/repo/requestManageRepo.js
@@ -2,7 +2,7 @@ var db=require('../fn/mysql-db');
 
 exports.loadAllRequest=()=>{
 	var sql=`select cd.IDCD,cd.IDTX,cd.DIEMDI,cd.THOIGIANDAT,cd.TOADON as CDTOADON,cd.TOADOW as CDTOADOW,
-				cd.GHICHU,cd.HOTENKH,cd.SDT as SDTKH,cd.STATECD,cd.REVERGEOCOD,cd.TIMEUPDATE,cd.STATEREQUEST as STATEREQUES,
+				cd.GHICHU,cd.HOTENKH,cd.SDT as SDTKH,cd.STATECD,cd.REVERGEOCODING as REVERGEOCOD,cd.TIMEUPDATE,cd.STATEREQUEST as STATEREQUES,
 				tx.ID,tx.HOTEN,tx.SDT ,tx.NGAYSINH,tx.CMND,tx.DIACHI,tx.STATE,tx.TOADON,tx.TOADOW
 				from chuyendi cd left join taixe tx on cd.IDTX=tx.ID`;
 	return db.load(sql);
@@ -10,7 +10,7 @@ exports.loadAllRequest=()=>{
 
 exports.getNew=(lrt)=>{
 	var sql=`select cd.IDCD,cd.IDTX,cd.DIEMDI,cd.THOIGIANDAT,cd.TOADON as CDTOADON,cd.TOADOW as CDTOADOW,
-				cd.GHICHU,cd.HOTENKH,cd.SDT as SDTKH,cd.STATECD,cd.REVERGEOCOD,cd.TIMEUPDATE,cd.STATEREQUEST as STATEREQUES,
+				cd.GHICHU,cd.HOTENKH,cd.SDT as SDTKH,cd.STATECD,cd.REVERGEOCODING as REVERGEOCOD,cd.TIMEUPDATE,cd.STATEREQUEST as STATEREQUES,
 				tx.ID,tx.HOTEN,tx.SDT ,tx.NGAYSINH,tx.CMND,tx.DIACHI,tx.STATE,tx.TOADON,tx.TOADOW
 				from chuyendi cd left join taixe tx on cd.IDTX=tx.ID
 				  where cd.TIMEUPDATE >${lrt}`;
